6ab63812a5409372441fdd3233e038c29d64cc7d
DIEU Anthony (SNCF VOYAGEURS / TECHNICENTRE DE VENISSIEUX / Equ Maintenance) <8402956y@commun.ad.sncf.fr>
2023-03-13 16:30:25 +0100

    first commit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,29 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor doctor(@PathVariable String number) {
+        if (number.equals("13")) {
+            return new Doctor(13, "Jodie Whittaker");
+        } else if (Integer.parseInt(number) > 13) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
 
-        return null;
+        } else {
+
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER);
+        }
     }
+
 }
